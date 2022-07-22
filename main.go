a57d764c8971d5d285df83a93421ad3b0d6ab5eb
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-07-22 23:39:49 +0530

    -Implemented http server

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func main() {
+func ExecuteClient() {
 	resp, err := http.Get("https://gobyexample.com")
 	if err != nil {
 		panic(err)
@@ -23,4 +23,27 @@ func main() {
 	if err := Scanner.Err(); err != nil {
 		panic(err)
 	}
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func header(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v", name, h)
+		}
+	}
+}
+
+func ExecuteServer() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/header", header)
+	http.ListenAndServe(":8090", nil)
+}
+
+func main() {
+	// ExecuteClient()
+	ExecuteServer()
 }
