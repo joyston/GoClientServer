31d5c5dd321a20563a952ff1d348d3a1664ae328
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-08-11 01:25:00 +0530

    -Redirecting -Url not found

--- Server/jServer.go
+++ Server/jServer.go
@@ -20,13 +20,25 @@ func header(w http.ResponseWriter, req *http.Request) {
 }
 
 func subtreepath(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("<h1>This is the about page</h1>"))
+}
+
+func Redirect(url string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, url, 301)
+	}
 }
 
 func ExecuteServer() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", subtreepath)
-	mux.HandleFunc("/hello", hello)
-	mux.HandleFunc("/header", header)
+	mux.HandleFunc("/", subtreepath)  //Subtree path
+	mux.HandleFunc("/hello", hello)   //Fixed path
+	mux.HandleFunc("/header", header) //Fixed path
+	mux.HandleFunc("/example", Redirect("http://example.com"))
 	http.ListenAndServe(":8090", mux)
 }
